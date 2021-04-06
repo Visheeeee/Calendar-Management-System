662d011dd25b43779047e7d05b47cf9cf5dce145
navaneethM1 <navaneethmukund27@gmail.com>
2021-04-06 16:26:23 +0530

    add email remainder functionality

--- OOADSE/app.py
+++ OOADSE/app.py
@@ -7,6 +7,13 @@ import os
 import datetime
 import json
 import calendar
+import threading
+from socket import *
+from base64 import b64encode, b64decode
+import os
+from datetime import datetime
+
+      
 
 import sqlite3
 
@@ -115,6 +122,8 @@ def loggedup():
                 if len(rows)>=1: 
                     global mail_id
                     mail_id=mail
+                    t1 = threading.Thread(target=send_mail, args=(mail_id,))
+                    t1.start()
                     return render_template("result_login.html",mail=mail,pswd=pswd)
 
         except:
@@ -247,6 +256,146 @@ def login():
 
 
 
+def send_mail(mail_id):
+  # print('mail func started')
+  while(True):
+
+    f = open('./events/' + mail_id + '.json', 'r+')
+    j = json.loads(f.read())
+
+    for i in range(len(j)):
+      if j[i]['reminder'] == 'yes':
+        # print('entered if yes')
+        now = datetime.now()
+        startdate = list(map(int, j[i]['start'].split('-')))
+
+        startevent = datetime(startdate[0], startdate[1], startdate[2])
+        print(startevent - now)
+        print((startevent - now).days)
+        if((startevent-now).days <= 1):
+          print('-----------Email ready to send--------')
+          j[i]['reminder'] = 'no'
+
+          endmsg = '\r\n.\r\n'
+
+          # Sender - email, password and Receiver - email DETAILS
+          sender_email = 'navaneethmukund.nm@gmail.com'
+          sender_password = 'navaneeth123'
+          receiver_email = mail_id
+          # Encoding sender email and password in base64 format
+          encoded_sender_email = b64encode(sender_email.encode('ascii'))
+          encoded_sender_password = b64encode(sender_password.encode('ascii'))
+
+          # Choose a mail server (e.g. Google mail server) and call it mailserver
+          mailserver = 'smtp.gmail.com'
+
+          # Create socket called clientSocket
+          clientSocket = socket(AF_INET, SOCK_STREAM)
+
+          # Establish a TCP connection with mailserver (Port number may change according to the mail server)
+          clientSocket.connect((mailserver, 587))
+          recv = clientSocket.recv(1024).decode()
+          print (recv)
+          if recv[:3] != '220':
+            print('220 reply not received from server.')
+
+          # Send HELO command and print server response.
+          heloCommand = 'HELO google\r\n'
+          clientSocket.send(heloCommand.encode())
+          recv1 = clientSocket.recv(1024).decode()
+          print (recv1)
+          if recv1[:3] != '250':
+            print('250 reply not received from server.')
+
+          # STARTTLS
+          startTls = 'starttls auth login\r\n'
+          clientSocket.send(startTls.encode())
+          recv2 = clientSocket.recv(1024).decode()
+          print (recv2)
+          if recv2[:3] != '555':
+            print('555 reply not received from server.')
+
+          # Auth login
+          authLogin = 'auth login\r\n'
+          clientSocket.send(authLogin.encode())
+
+          # This is a base64 encoded string asking for username
+          recv3 = clientSocket.recv(1024)
+          if recv3.decode()[:3] != '334':
+            print('334 reply not received from server.')
+          print(recv3.decode().strip() + ' => gmail asking for ' + b64decode(recv3.split()[1]).decode('ascii')[:-1] + '\n')
+          # Sending sender_email
+          clientSocket.send(encoded_sender_email + '\r\n'.encode())
+
+          # This is a base64 encoded string asking for password
+          recv4 = clientSocket.recv(1024)
+          if recv4.decode()[:3] != '334':
+            print('334 reply not received from server.')
+          print(recv4.decode().strip() + ' => gmail asking for ' + b64decode(recv4.split()[1]).decode('ascii')[:-1] + '\n')
+          # Sending sender_password
+          clientSocket.send(encoded_sender_password + '\r\n'.encode())
+
+          # Looking for 235 Accepted response
+          recv5 = clientSocket.recv(1024).decode()
+          print (recv5)
+          if recv5[:3] != '235':
+            print('235 reply not received from server. Probably Auth login unsuccessful')
+
+          # Send MAIL FROM command and print server response.
+          mailfrom = 'MAIL FROM: <' + sender_email + '>\r\n'
+          clientSocket.send(mailfrom.encode())
+          recv6 = clientSocket.recv(1024).decode()
+          print (recv6)
+          if recv6[:3] != '250':
+            print('250 reply not received from server.')
+
+          # Send RCPT TO command and print server response. 
+          rcptto = 'RCPT TO: <' + receiver_email + '>\r\n'
+          clientSocket.send(rcptto.encode())
+          recv7 = clientSocket.recv(1024).decode()
+          print (recv7)
+          if recv7[:3] != '250':
+            print('250 reply not received from server.')
+
+          # Send DATA command and print server response. 
+          data = 'DATA\r\n'
+          clientSocket.send(data.encode())
+          recv8 = clientSocket.recv(1024).decode()
+          print (recv8)
+          if recv8[:3] != '354':
+            print('354 reply not received from server.')
+
+          # Send message data.
+          clientSocket.send('SUBJECT: Event Remainder!\r\n'.encode())
+          email_msg = j[i]['title'] + ' is scheduled tomorrow'
+          clientSocket.send(email_msg.encode())
+
+          # Message ends with a single period.
+          clientSocket.send(endmsg.encode())
+          recv9 = clientSocket.recv(1024).decode()
+          print (recv9)
+          if recv9[:3] != '250':
+            print('250 reply not received from server.')
+
+          # Send QUIT command and get server response.
+          quitcommand = 'QUIT\r\n'
+          clientSocket.send(quitcommand.encode())
+          recv10 = clientSocket.recv(1024).decode()
+          print (recv10)
+          if recv10[:3] != '221':
+            print('221 reply not received from server.')
+
+          # Closing TCP Connection
+          clientSocket.close()
+    f.seek(0)
+    f.truncate()
+    json.dump(j, f)
+    f.close()
+
+
+
+
+
 if __name__ == '__main__':
 
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="localhost", port=5000, debug=True)
